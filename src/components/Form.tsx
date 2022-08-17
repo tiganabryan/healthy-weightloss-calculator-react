3d5b1193b03b13e5baf2064c87ae156155371ed4
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-08-17 12:15:10 -0400

    add step 1 state to Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -19,31 +19,43 @@ import {
     Center
   } from '@chakra-ui/react';
 import { Stepper } from './Stepper';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
   
   
   
   export default function Form() {
 
-    const [userInput, setUserInput] = useState({
-      age: 0,
-      height: 0,
-      weight: 0,
-      activityLevel: 0,
-      dietLength: 0,
-      bmr: 0,
-      tdee: 0,
-    })
+  const [age, setAge] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [weight, setWeight] = useState(0)
+  const [gender, setGender] = useState('female')
 
-    interface userInput {
-      age: number,
-      height: number,
-      weight: number,
-      activityLevel: number,
-      dietLength: number,
-      bmr: number,
-      tdee: number,
-    }
+  const inputs = {
+      age: age,
+      height: height,
+      weight: weight,
+      gender: gender,
+  }
+
+    // const [userInput, setUserInput] = useState({
+    //   age: 0,
+    //   height: 0,
+    //   weight: 0,
+    //   activityLevel: 0,
+    //   dietLength: 0,
+    //   bmr: 0,
+    //   tdee: 0,
+    // })
+
+    // interface userInput {
+    //   age: number,
+    //   height: number,
+    //   weight: number,
+    //   activityLevel: number,
+    //   dietLength: number,
+    //   bmr: number,
+    //   tdee: number,
+    // }
 
     
 

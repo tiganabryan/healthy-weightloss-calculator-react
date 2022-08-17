6d1f270e90aac1cba68b9a48bd547f92d40e9913
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-08-16 23:26:01 -0400

    dynamically update object with user input

--- src/components/Step1.tsx
+++ src/components/Step1.tsx
@@ -24,7 +24,7 @@ const Step1 = () => {
   const [age, setAge] = useState(0)
   const [height, setHeight] = useState(0)
   const [weight, setWeight] = useState(0)
-  const [gender, setGender] = useState(0)
+  const [gender, setGender] = useState('female')
 
   const inputs = {
       age: age,
@@ -36,18 +36,18 @@ const Step1 = () => {
 
   useEffect(() => {
       console.log(inputs)
-      console.log(inputs.age + inputs.height)
+      console.log(inputs)
   }, [inputs])
 
   return (
     <Box w='100%' p={4} mt={4} borderRadius={12} borderColor='navy' borderWidth={1}>
         <form>
         <Stack spacing={3}>
-            <Input type="number" placeholder='age' size='lg' />
-            <Input type="number" placeholder='height' size='lg' />
-            <Input type="number" placeholder='weight' size='lg' />
+            <Input type="number" placeholder='age' onChange={(e) => setAge(Number(e.target.value))} size='lg' />
+            <Input type="number" placeholder='height' onChange={(e) => setHeight(Number(e.target.value))} size='lg' />
+            <Input type="number" placeholder='weight' onChange={(e) => setWeight(Number(e.target.value))} size='lg' />
 
-            <Select placeholder='gender' size='lg'>
+            <Select placeholder='gender' onChange={(e) => setGender(e.target.value)} size='lg'>
                 <option value='male'>male</option>
                 <option value='female'>female</option>
                 <option value='other'>other</option>

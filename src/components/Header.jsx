82511759e146a6bb371511216add708085d26ebf
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-09-21 10:06:28 -0400

    add responsive styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ import {
 const Header = () => {
   return (
     <React.Fragment>
-        <Text fontSize='5xl' textAlign='center'>varied intake weight loss calculator</Text>
-            <Text fontSize='3xl' textAlign='center'>life is ever changing. why isn't your diet?</Text>
+        <Text fontSize={{base: '3xl', md: '5xl', lg: '4xl'}} textAlign='center' padding={0} fontWeight={{base: 500}}>varied intake weight loss calculator</Text>
+            <Text fontSize={{base: '2xl', md: '3xl', lg: '3xl'}} textAlign='center'>life is ever changing. why isn't your diet?</Text>
     </React.Fragment>
   )
 }

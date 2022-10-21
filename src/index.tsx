e329f89c4b3634e4fd8f1d5e3b11084aeff4a2ac
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-10-21 11:16:03 -0400

    provide redux store to react

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
+import { store } from "./store/store"
+import { Provider } from 'react-redux'
 
 
 const container = document.getElementById("root")
@@ -13,7 +15,9 @@ const root = ReactDOM.createRoot(container)
 root.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
   </React.StrictMode>,
 )
 
